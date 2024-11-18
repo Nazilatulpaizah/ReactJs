76cb1be2232946d43d30ca047c3d5a894d282b45
nazila <nazilatulfaizah011@gmail.com>
2024-11-18 20:45:52 +0700

    first commit

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,101 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'PRODUCT-001',
+      slug: 'bj1',
+      name: "Blouse Cream Wanita Korea",
+      category: 'Best Seller',
+      price: 150_000,
+      stock: 25,
+      imageUrl: '/assets/images/bj1.jpg',
+      size: "M",
+      color: "White",
+      
+    },
+    {
+      id: 'PRODUCT-002',
+      slug: 'bj2',
+      name: "Blouse Wanita",
+      category: 'Best Seller',
+      price: 150_000,
+      stock: 15,
+      imageUrl: '/assets/images/bj2.jpg',
+      size: "M",
+      color: "White",
+      
+    },
+    {
+      id: 'PRODUCT-003',
+      slug: 'bj3',
+      name: "Blouse Korea",
+      category: 'Popular',
+      price: 120_000,
+      stock: 50,
+      imageUrl: '/assets/images/bj3.jpg',
+      size: "XL",
+      color: "Pink",
+      
+    },
+    {
+      id: 'PRODUCT-004',
+      slug: 'bj4',
+      name: "Tunik Wanita",
+      category: 'Popular',
+      price: 150_000,
+      stock: 45,
+      imageUrl: '/assets/images/bj4.jpg',
+      size: "M",
+      color: "White",
+      
+    },
+    {
+      id: 'PRODUCT-005',
+      slug: 'bj5',
+      name: "Katun Wanita",
+      category: 'Best Seller',
+      price: 100_000,
+      stock: 10,
+      imageUrl: '/assets/images/bj5.jpg',
+      size: "S",
+      color: "Black",
+      
+    },
+    {
+      id: 'PRODUCT-006',
+      slug: 'bj6',
+      name: "Tunik Toyobo",
+      category: 'Popular',
+      price: 108_000,
+      stock: 35,
+      imageUrl: '/assets/images/bj6.jpg',
+      size: "S",
+      color: "Cream",
+      
+    },
+    {
+      id: 'PRODUCT-007',
+      slug: 'bj7',
+      name: "Blouse Kantor",
+      category: 'Best Seller',
+      price: 80_000,
+      stock: 20,
+      imageUrl: '/assets/images/bj7.jpg',
+      size: "L",
+      color: "Pink",
+      
+    },
+    {
+      id: 'PRODUCT-008',
+      slug: 'bj8',
+      name: "Blouse Motif Segitiga",
+      category: 'Popular',
+      price: 150_000,
+      stock: 40,
+      imageUrl: '/assets/images/bj8.jpg',
+      size: "M",
+      color: "White",
+      
+    },
+
+  ]
+}
